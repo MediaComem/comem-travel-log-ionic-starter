fd6aeeb51707e6cf2feb0fad47363105501d835a
Simon Oulevay <simon.oulevay@gmail.com>
2018-12-17 17:08:22 +0100

    Geolocation, map & camera examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +17,7 @@ import { TripListPage } from '../pages/trip-list/trip-list';
 import { AuthProvider } from '../providers/auth/auth';
 import { LoginPage } from '../pages/login/login';
 import { AuthInterceptorProvider } from '../providers/auth-interceptor/auth-interceptor';
+import { DetailsPage } from '../pages/details/details';
 
 @NgModule({
   declarations: [
@@ -22,13 +26,15 @@ import { AuthInterceptorProvider } from '../providers/auth-interceptor/auth-inte
     CreateTripPage,
     PlacesMapPage,
     TripListPage,
-    LoginPage
+    LoginPage,
+    DetailsPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     HttpClientModule,
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    LeafletModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -37,7 +43,8 @@ import { AuthInterceptorProvider } from '../providers/auth-interceptor/auth-inte
     CreateTripPage,
     PlacesMapPage,
     TripListPage,
-    LoginPage
+    LoginPage,
+    DetailsPage
   ],
   providers: [
     StatusBar,
@@ -45,7 +52,9 @@ import { AuthInterceptorProvider } from '../providers/auth-interceptor/auth-inte
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorProvider, multi: true },
     AuthProvider,
-    AuthInterceptorProvider
+    AuthInterceptorProvider,
+    Geolocation,
+    Camera
   ]
 })
 export class AppModule {}
--- src/pages/create-trip/create-trip.ts
+++ src/pages/create-trip/create-trip.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { AuthProvider } from '../../providers/auth/auth';
+import { DetailsPage } from '../details/details';
 
 /**
  * Generated class for the CreateTripPage page.
@@ -15,16 +18,45 @@ import { AuthProvider } from '../../providers/auth/auth';
   templateUrl: 'create-trip.html',
 })
 export class CreateTripPage {
+  pictureData: string;
 
-  constructor(private auth: AuthProvider, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private camera: Camera, private geolocation: Geolocation, private auth: AuthProvider, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateTripPage');
+
+    const geolocationPromise = this.geolocation.getCurrentPosition();
+    geolocationPromise.then(position => {
+      const coords = position.coords;
+      console.log(`User is at ${coords.longitude}, ${coords.latitude}`);
+    }).catch(err => {
+      console.warn(`Could not retrieve user position because: ${err.message}`);
+    });
+  }
+
+  goToDetails() {
+    this.navCtrl.push(DetailsPage, { id: 23 });
   }
 
   logOut() {
     this.auth.logOut();
+  }
+
+  takePicture() {
+    
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(pictureData => {
+      this.pictureData = pictureData;
+    }).catch(err => {
+      console.warn(`Could not take picture because: ${err.message}`);
+    });
   }
 
 }

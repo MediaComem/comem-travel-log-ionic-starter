7b9ec455fa15211232bc19354fe2b7bdd15cbbf4
Mathias Oberson <mathias@squareservices.ch>
2019-11-04 17:17:13 +0100

    Use storage to store/remove authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, ReplaySubject, from } from 'rxjs';
+import { delayWhen, map } from 'rxjs/operators';
+
+import { Storage } from '@ionic/storage';
 
 import { AuthResponse } from '../models/auth-response';
 import { User } from '../models/user';
@@ -16,10 +18,16 @@ export class AuthService {
   private auth$: Observable<AuthResponse>;
   private authSource: ReplaySubject<AuthResponse>;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private storage: Storage
+  ) {
     this.authSource = new ReplaySubject(1);
-    this.authSource.next(undefined);
     this.auth$ = this.authSource.asObservable();
+
+    this.storage.get('auth').then(auth => {
+      this.authSource.next(auth);
+    });
   }
 
   isAuthenticated(): Observable<boolean> {
@@ -38,6 +46,9 @@ export class AuthService {
 
     const authUrl = 'https://comem-travel-log-api.herokuapp.com/api/auth';
     return this.http.post<AuthResponse>(authUrl, authRequest).pipe(
+      delayWhen(auth => {
+        return this.saveAuth(auth);
+      }),
       map(auth => {
         this.authSource.next(auth);
         console.log(`User ${auth.user.name} logged in`);
@@ -48,7 +59,11 @@ export class AuthService {
 
   logOut() {
     this.authSource.next(null);
+    this.storage.remove('auth');
     console.log('User logged out');
   }
 
+  private saveAuth(auth: AuthResponse): Observable<void> {
+    return from(this.storage.set('auth', auth));
+  }
 }
--- src/app/home/create-trip/create-trip.page.ts
+++ src/app/home/create-trip/create-trip.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-create-trip',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTripPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  logOut() {
+    console.log('logging out...');
+    this.auth.logOut();
+    this.router.navigateByUrl('/login');
   }
 
 }
